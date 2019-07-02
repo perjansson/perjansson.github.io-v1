f002b2c5f172968e589ae1c32bc2344aa1bec666
Per Jansson <per.r.jansson@gmail.com>
2019-07-02 22:14:39 +0300

    More projects

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ var content = {
         name: 'Per Jansson',
         title: 'Fullstack Web Developer',
         short:
-            "Hi I'm Per, a curious software developer with a passion to build great stuff and help others do the same.",
+            "Hi I'm Per, a <strong>curious</strong> software developer with a passion to <strong>build great stuff</strong> and help others do the same.",
         long:
-            'My core skills are in fullstack web development and, although I’m no stranger to backend, I’ve always been drawn to the frontend side. I like the things that are visual and that a user interacts with and I have many years of experience in how to build systems and applications that are a good mix of quality, robustness and ease of use. I really enjoy working with Node, React and JavaScript in general, but I also have done a lot of Java previously together with both Android och iOS development. I addition to that I also like the architectural part of how a solid frontend is created together with making the Developer Experience really good for the current team so it’s a joy to work with.'
+            'My core skills are in fullstack <strong>web development</strong> and, although I’m no stranger to backend, I’ve always been drawn to the <strong>frontend</strong> side. I like the things that are visual and that a user interacts with and I have many years of experience in how to build systems and applications that are a good mix of <strong>quality, robustness and ease of use</strong>. I really enjoy working with <strong>Node, React and JavaScript</strong> in general, but I also have done a lot of Java previously together with both Android och iOS development. I addition to that I also like the architectural part of how a solid frontend is created together with making the <strong>Developer Experience</strong> really good for the current team so it’s a joy to work with.'
     },
     projects: [
         {
@@ -25,6 +25,7 @@ var content = {
                 'node',
                 'npm',
                 'git',
+                'jenkins',
                 'vs code'
             ]
         },
@@ -54,24 +55,8 @@ var content = {
                 'codemods',
                 'jscodeshift',
                 'git',
+                'jenkins',
                 'vs code'
-            ]
-        },
-        {
-            title: 'New Admin web for Leadenhancer',
-            description:
-                'Web application for the back office users to configure different tracking setups for difference clients.',
-            me:
-                'Frontend developer building the new admin web to support the needs of the admin users of Leadenhancer. Performed a technical migration of the project including the build system (switching Gulp to Webpack and as well as from Bower to NPM), code quality (switching jshint to ESLint and added Prettier for format rules) and javascript (migrated AngularJS from ES5 to ES2015 and beyond). Complete restyling of the application to a more modern look and feel.',
-            tags: [
-                'javascript',
-                'angular',
-                'node',
-                'npm',
-                'sass',
-                'eslint',
-                'prettier',
-                'webpack'
             ]
         },
         {
@@ -106,22 +91,267 @@ var content = {
             ]
         },
         {
-            title: '',
-            description: '',
-            me: '',
-            tags: ''
+            title: 'New Admin web for Leadenhancer',
+            description:
+                'Web application for back office of company that bridges the gap between known and anonymous web visitors by identifying and segmenting the companies visiting a website, and providing detailed, targetable business attributes in real-time.',
+            me:
+                'Frontend responsible to build a new admin web to support the needs of the admin users of Leadenhancer. Involves everything from architecture and build and deploy pipeline, REST api design to coding the bits and pieces that are a modern web application.',
+            tags: [
+                'javascript',
+                'angular',
+                'node',
+                'npm',
+                'sass',
+                'eslint',
+                'prettier',
+                'webpack'
+            ]
+        },
+        {
+            title: 'Digitalization and Advisory Cockpit Tool for Nordea Bank',
+            description:
+                "Advisory Cockpit Tool to be used by advisors and help digitize an advisor's everyday. The bar was set very high and the application should not only contain a lot of functionality to simplify the days of an advisor, but the end result will also be a great user experience.",
+            me:
+                'Development and architecture of the frontend and BFF part of the application. Setting up fast and solid development experience and in general building the foundation of an easy to develop and maintain system. Daily work with building pages and components together with API design.',
+            tags: [
+                'javascript',
+                'react',
+                'redux',
+                'reselect',
+                'express',
+                'node',
+                'npm',
+                'webpack',
+                'babel',
+                'lodash',
+                'html',
+                'material-ui',
+                'sass',
+                'css modules',
+                'standardjs',
+                'mocha',
+                'chai',
+                'sinon',
+                'enzyme',
+                'faker.js',
+                'moment',
+                'postman',
+                'java',
+                'spring',
+                'jackson',
+                'scrum',
+                'tdd',
+                'jenkins'
+            ]
+        },
+        {
+            title: 'Mindmend prototype',
+            description:
+                'A prototype for a digital platform for KBT treatment.',
+            me:
+                'Developer of a responsive web app, everything from Sketch to running in the cloud.',
+            tags: [
+                'javascript',
+                'html',
+                'jquery',
+                'materialize',
+                'sass',
+                'animate.css',
+                'express',
+                'node',
+                'npm',
+                'webpack',
+                'heroku',
+                'agile'
+            ]
+        },
+        {
+            title: 'Nordic portfolio system for Nordea Bank',
+            description:
+                'A brand new back office solution along with enhancing and introducing an existing portfolio system in the Nordic countries',
+            me:
+                'Coding frontend architect developing the frontend tier, setting up fast and solid development practices/processes/style guides and build system together with being a happy team player working very close to the end user to guarantee the best result.',
+            tags: [
+                'javascript',
+                'angularjs',
+                'es2015',
+                'node',
+                'npm',
+                'gulp',
+                'bower',
+                'jquery',
+                'underscore',
+                'html',
+                'bootstrap',
+                'sass',
+                'jshint',
+                'csslint',
+                'htmlhint',
+                'karma',
+                'protractor',
+                'mocha',
+                'chai',
+                'sinon',
+                'java',
+                'spring',
+                'hibernate',
+                'dozer',
+                'flying saucer',
+                'intellij',
+                'git',
+                'maven',
+                'jenkins',
+                'sonarqube',
+                'sql server',
+                'tdd',
+                'ddd',
+                'kanban',
+                'xp'
+            ]
+        },
+        {
+            title: 'Risk Calculator for Nordea Bank',
+            description:
+                "A system for calculating risk and return, both based on a customer's current portfolio but also on a better allocated portfolio that will be recommended to the customer.",
+            me:
+                'Full stack developer of the entire system, although primarily responsible for frontend and API, together with being a happy team player working very close to the end user to guarantee the best result.',
+            tags: [
+                'angularjs',
+                'html',
+                'css',
+                'boostrap',
+                'karma',
+                'protractor',
+                'java',
+                'spring',
+                'hibernate',
+                'thymeleaf',
+                'flying Saucer',
+                'jensoft',
+                'dozer',
+                'intellij',
+                'git',
+                'maven',
+                'jenkins',
+                'sonarqube',
+                'sql server',
+                'tdd',
+                'ddd',
+                'scrum',
+                'xp'
+            ]
+        },
+        {
+            title: 'Multi portfolio for Nordea Bank',
+            description:
+                "Portfolio analysis system extended to support structuring a customer's total engagement in different portfolios to be able to twist and turn the customers current assets.",
+            me:
+                'Development and Scrum Master making sure that the team delivers what the end user expects with good code quality.',
+            tags: [
+                'gwt',
+                'java',
+                'spring',
+                'hibernate',
+                'apache camel',
+                'dozer',
+                'html',
+                'css',
+                'javascript',
+                'intellij',
+                'svn',
+                'maven',
+                'jenkins',
+                'sonarqube',
+                'sql Server',
+                'tdd',
+                'ddd',
+                'scrum',
+                'xp'
+            ]
+        },
+        {
+            title: 'Customized analysis and recommendation for Nordea Bank',
+            description:
+                "A system for doing customized analysis, optimization and recommendation of customer's current portfolio to be able to recommend a better portfolio that can give a higher return with a lower risk. <a href='https://medium.com/@perjansson/offshoring-from-the-scream-to-friendship-and-success-5b409c30d287' target='_blank' rel='noopener noreferrer'>Blog post about project on Medium</a>.",
+            me:
+                'Development and architecture of the system. Being UX and front end responsible to make the system easy and fast to use. Be proactive in efforts to get a distributed team between Sweden, Denmark and India to work.',
+            tags: [
+                'gwt',
+                'java',
+                'spring',
+                'hibernate',
+                'dozer',
+                'html',
+                'css',
+                'javascript',
+                'eclipse',
+                'svn',
+                'maven',
+                'jenkins',
+                'sonarqube',
+                'sql Server',
+                'tdd',
+                'ddd',
+                'scrum',
+                'xp',
+                'lean'
+            ]
+        },
+        {
+            title: 'Portfolio for advisors for Nordea Bank',
+            description:
+                'A web application for advisors to use when doing analysis of a customers holdings, for instance asset allocation, current and previous holdings, transactions, performance and profit and loss. <a href="https://medium.com/@perjansson/how-we-invented-and-introduced-drama-driven-demo-9cc564bc741f" target="_blank" rel="noopener noreferrer">Blog post about project on Medium</a>.',
+            me:
+                'Development and UX making sure the system makes sense and is easy to use, looks nice and is quick and responsive. Advocate efficient software development practices such as pair programming, TDD, code reviews and brown bag lunches.',
+            tags: [
+                'gwt',
+                'java',
+                'spring',
+                'hibernate',
+                'dozer',
+                'html',
+                'css',
+                'javascript',
+                'eclipse',
+                'svn',
+                'maven',
+                'jenkins',
+                'sonarqube',
+                'sql Server',
+                'tdd',
+                'ddd',
+                'scrum',
+                'xp',
+                'lean'
+            ]
+        },
+        {
+            title: 'App on Android for Swedish Pharmacy',
+            description:
+                'An Android app to view the current pollen level at a given location as well as recommended treatments and view closest pharmacy.',
+            me:
+                'Develop the Apoteket (swedish pharmacy) application for Android. Took the project from requirements and development to testing and release on Google Play.',
+            tags: ['android', 'java', 'android studio', 'scrum']
+        },
+        {
+            title: 'App on Android for Statoil',
+            description:
+                'An Android app to view the closest Statoil gas station as well as book trailers, vans etc.',
+            me:
+                'Develop the Swedish Statoil application for Android. Took the project from requirements and development to testing and release on Google Play.',
+            tags: ['android', 'java', 'android studio', 'scrum']
         },
         {
             title: '',
             description: '',
             me: '',
-            tags: ''
+            tags: []
         },
         {
             title: '',
             description: '',
             me: '',
-            tags: ''
+            tags: []
         }
     ]
 };
@@ -185,41 +415,41 @@ content.projects
 
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.me__header', {
-    distance: '150%',
-    origin: 'top',
-    duration: 1000,
-    delay: 500
-});
+setTimeout(function() {
+    sr.reveal('.me__header', {
+        distance: '150%',
+        origin: 'top',
+        duration: 1000
+    });
 
-sr.reveal('.me__description', {
-    scale: 0.9,
-    duration: 1000,
-    delay: 1500
-});
+    sr.reveal('.me__description', {
+        scale: 0.9,
+        duration: 1000,
+        delay: 1000
+    });
 
-sr.reveal('.me__more', {
-    distance: '100%',
-    origin: 'left',
-    duration: 1000,
-    delay: 2500
-});
+    sr.reveal('.me__more', {
+        distance: '100%',
+        origin: 'left',
+        duration: 1000,
+        delay: 2000
+    });
 
-sr.reveal('.projects');
+    sr.reveal('.projects');
 
-sr.reveal('.projects__header', {
-    scale: 0.9,
-    duration: 1000,
-    delay: 2500
-});
+    sr.reveal('.projects__header', {
+        scale: 0.9,
+        duration: 1000,
+        delay: 2000
+    });
 
-sr.reveal('.project', {
-    distance: '25%',
-    origin: 'bottom',
-    scale: 0.8,
-    duration: 1000,
-    delay: 500
-});
+    sr.reveal('.project', {
+        distance: '25%',
+        origin: 'bottom',
+        scale: 0.8,
+        duration: 1000
+    });
+}, 500);
 
 var projectsHeader = document.querySelector('.projects .projects__header');
 
