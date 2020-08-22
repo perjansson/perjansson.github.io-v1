6c9810e538991aac467ad8defdbb4a57616d00a3
Per Jansson <per.r.jansson@gmail.com>
2020-08-22 23:11:38 +0300

    Connectivity background

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,49 @@
 <script>
+  import { onMount } from "svelte";
+
   import content from "./content";
   import ProjectList from "./components/ProjectList.svelte";
   import Me from "./components/Me.svelte";
+
+  onMount(() => {
+    window.addEventListener("online", handleConnection);
+    window.addEventListener("offline", handleConnection);
+
+    async function handleConnection() {
+      if (navigator.onLine) {
+        const isConnected = await hasConnection();
+        if (isConnected) {
+          // Connected
+          setConnectionStyles(true);
+        } else {
+          // Not connected
+          setConnectionStyles(false);
+        }
+      } else {
+        // Offline
+        setConnectionStyles(false);
+      }
+    }
+
+    async function hasConnection() {
+      try {
+        await fetch("https://google.com/noexist", {
+          method: "HEAD",
+          mode: "no-cors"
+        });
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+
+    function setConnectionStyles(connected) {
+      document.documentElement.style.setProperty(
+        "--connectivity-color",
+        connected ? "transparent" : "gray"
+      );
+    }
+  });
 </script>
 
 <style>
